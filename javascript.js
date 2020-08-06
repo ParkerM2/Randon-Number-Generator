f090ddcea88032b85e4d4ffb174e878786cb90ee
Parker Manning <parkerlmanning@hotmail.com>
2020-08-06 01:00:30 -0500

    pushes to console

--- javascript.js
+++ javascript.js
@@ -12,14 +12,14 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Prompts //
-var charLength = parseInt(prompt("Between 8-128 characters, how many would you like?"));
+var charLength = prompt("Between 8-128 characters, how many would you like?");
 var upCase = confirm("Would you like Upper Case letters?");
 var lowCase = confirm("Would you like lower case letters?");
 var numb = confirm("Would you like numbers?");
 var sym = confirm("Would you like symbols?");
 
 
-console.log(charLength);
+
 
 // Functions for Generating random letters/symbols/numbers //
 
@@ -28,37 +28,142 @@ console.log(charLength);
 //  for letters * 26 + starting number on list, for numbers * 10 + starting number //
 
 // reminder *** DRY CODE *** //
-
+// var = letters ["a", "b", "c"];
 
 // generating password //
 
 
-function generatePassword() {
-    for (i = 0; i < charLength; i++) {
+function generatePassword(writePassword) {
+   
+    for (var i = 0; i < charLength; i++) {
 
-        // retrieving random lower case letter //
-        if (lowCase === true) {
-            return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+        if (upCase === true) {
+            writeUpperCase();
+            i++;
         }
+        else if (upCase === false) {
+        };
         
-        // retrieving random upper case letter //
-        else if (upCase === true) {
-            return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-            
+        if (lowCase === true) {
+            writeLowerCase();
+            i++;
         }
-
-        // retrieving random number //
-        else if (numb === true) {
-            return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-            
+        else if (lowCase === false) {
+        };
+        if (numb === true) {
+            writeRanNumber();
+            i++;
         }
-            
-        // retrieving random symbol //
-        else if (sym === true) {
-            const symbols = '!@#$%^&*(){}[]=<>/,.';
-            return symbols[Math.floor(Math.random() * symbols.length)];
+        else if (numb === false) {
+        };
+        if (sym === true) {
+            writeRanSymbol();
+            i++;
         }
+        else if (sym === false) {
+        };
     }
-}
     
+            function writeLowerCase() {
+                var text = "";
+                const lettersL = 'abcdefghijklmnopqrstuvxyz';
+                text += lettersL[Math.floor(Math.random() * lettersL.length)];
+                console.log(text);
+                
+            }
 
+            function writeUpperCase() {
+                var text = "";
+                const lettersU = 'ABCDEFGHIJKLMNOPQRSTUVXYZ';
+                text += lettersU[Math.floor(Math.random() * lettersU.length)];
+                console.log(text);
+                
+            }
+
+            function writeRanNumber() {
+                var text = "";
+                const numbers = '1234567789';
+                text += numbers[Math.floor(Math.random() * 9)];
+                console.log(text)
+                
+            }
+
+            function writeRanSymbol() {
+                var text = "";
+                const symbols = '!@#$%^&*()';
+                text += symbols[Math.floor(Math.random() * 10)];
+                console.log(text)
+            }
+}
+
+
+//         // lower case only
+//         if (lowCase === true && upCase === false && numb === false && sym === false) {
+//             writeLowerCase();
+        
+//         }
+
+//         // lowercase, uppercase only
+//         else if (lowCase === true && upCase === true && numb === false && sym === false) {
+//             writeLowerCase();
+//             writeUpperCase();
+//             console.log(i)
+//         }
+            
+//         // lowercase, uppercase, number only
+//         else if (lowCase === true && upCase === true && numb === true && sym === false) {
+//             writeLowerCase();
+//             writeUpperCase();
+//             writeRanNumber();
+//         }
+            
+//         // lowercase, uppercase, number, symbol (all)
+//         else if (lowCase === true && upCase === true && numb === true && sym === true) {
+//             writeLowerCase();
+//             writeUpperCase();
+//             writeRanNumber();
+//             writeRanSymbol();
+//         }
+            
+//         // all false
+//         else if (lowCase === false && upCase === false && numb === false && sym === false) {
+//             alert("No input no password!")
+//         }
+            
+//         // uppercase only
+//         else if (lowCase === false && upCase === true && numb === false && sym === false) {
+//             writeUpperCase();
+//         }
+            
+//         // uppercase, number only
+//         else if (lowCase === false && upCase === true && numb === true && sym === false) {
+        
+//         }
+
+//         // uppercase, number, symbol only
+//         else if (lowCase === false && upCase === true && numb === true && sym === true) {
+        
+//         }
+        
+//         // Uppercase, symbol only
+//         else if (lowCase === false && upCase === true && numb === false && sym === true) {
+        
+//         }
+
+
+//         // numb only
+//         else if (lowCase === false && upCase === false && numb === true && sym === false) {
+        
+//         }
+
+//         // numb, symb only
+//         else if (lowCase === false && upCase === false && numb === true && sym === true) {
+        
+//         }
+//         // symbol only
+//         else if (lowCase === false && upCase === false && numb === false && sym === true) {
+        
+//         }
+
+//     }
+// }
